c2369a4e26e8cf2798c4b07fdc7a5b592b884d01
yisas322 <jesus.truel@hotmail.com>
2024-07-05 10:44:43 -0500

    Add files via upload

--- Grifo_primax.java
+++ Grifo_primax.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 public class Grifo_primax {
 
 
-    private String[] usuarios;      // Array para almacenar nombres de usuario
-    private String[] correos;       // Array para almacenar correos electrónicos
-    private String[] contraseñas;   // Array para almacenar contraseñas
-    private int numUsuarios;        // Número de usuarios registrados
-    private Scanner scanner;        // Objeto Scanner para entrada de usuario
+    private String[] usuarios;
+    private String[] correos;
+    private String[] contraseñas;
+    private int numUsuarios;
+    private Scanner scanner;
 
     public Grifo_primax() {
-        usuarios = new String[10];      // Inicializamos los arrays con capacidad para 10 usuarios
+        usuarios = new String[10];
         correos = new String[10];
         contraseñas = new String[10];
-        numUsuarios = 0;                // Inicialmente no hay usuarios registrados
-        scanner = new Scanner(System.in);  // Inicializamos el Scanner para entrada estándar
+        numUsuarios = 0;
+        scanner = new Scanner(System.in);
     }public void opciones(){
         Scanner input = new Scanner(System.in);
         System.out.println("1.-Crear Cuenta");
@@ -39,15 +39,15 @@ public class Grifo_primax {
         System.out.print("Ingrese su contraseña: ");
         String contraseña = scanner.nextLine();
 
-        // Verificar si el correo ya está registrado
+
         for (int i = 0; i < numUsuarios; i++) {
             if (correos[i].equals(correo)) {
                 System.out.println("Error: El correo ya está registrado.");
-                return;  // Salir del método si el correo ya está registrado
+                return;
             }
         }
 
-        // Almacenar los datos del nuevo usuario en los arrays
+
         usuarios[numUsuarios] = usuario;
         correos[numUsuarios] = correo;
         contraseñas[numUsuarios] = contraseña;
@@ -63,11 +63,11 @@ public class Grifo_primax {
         System.out.print("Ingrese su contraseña: ");
         String contraseña_in = scanner.nextLine();
 
-        // Verificar si las credenciales coinciden con algún usuario registrado
+
         for (int i = 0; i < numUsuarios; i++) {
             if (correos[i].equals(correo_in) && contraseñas[i].equals(contraseña_in)) {
                 System.out.println("Bienvenido: " + usuarios[i]);
-              // Salir del método si se encuentra el usuario
+
             }
         }
 
